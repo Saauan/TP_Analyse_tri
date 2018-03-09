6d588a7dce551e9566797dafee8d984ba6ab047e
Saauan <36884364+Saauan@users.noreply.github.com>
2018-03-09 21:59:53 +0100

    Added time analysis, corrected bugs
    I stil have to comment it.

--- tp_tri.py
+++ tp_tri.py
@@ -6,7 +6,7 @@ import timeit
 import tris
 
 TAILLE_MAX = 100
-
+NUMBER_ITERATIONS = 500
 
 def liste_alea(n):
     """
@@ -31,22 +31,66 @@ def analyse_tri_temps(tri, n, t):
         l = liste_alea(t)
         tri(l)
 
-temps_select = []
-print("test1")
-with open("temps_tri_selec.csv", "w", encoding="utf-8") as canal_selec:
-    print("test2")
-    canal_selec.write('taille;"tri_selection"\n')
-    for taille in range(1, TAILLE_MAX+1):
-        temps = timeit.timeit(stmt='tris.tri_select(liste_alea(taille))',
-                              setup="import tris; from tp_tri import liste_alea, taille; from random import shuffle",
-                              number=50)
-        temps_select.append(temps)
-        canal_selec.write("{:3d} ; {:8f}\n".format(taille,temps))
-    print("test3")
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
+    temps_select = []
+    with open("temps_tri_selec.csv", "w", encoding="utf-8") as canal_selec:
+        canal_selec.write('taille;"tri_selection"\n')
+        
+        for taille in range(0, TAILLE_MAX+1): # Je sais que normalement, ce sont les etniers t de 1 à TAILLE_MAX, mais pour uniformiser avec les autres mesures, j'ai pris la liberté de rajouter 0 comme valeur possible de t
+            l = liste_alea(taille)
+            temps = timeit.timeit(stmt='tris.tri_select(l)',
+                                  setup="import tris; from __main__ import liste_alea, l, taille; from random import shuffle",
+                                  number=NUMBER_ITERATIONS)
+            temps_select.append(temps)
+            canal_selec.write("{:3d} ; {:8f}\n".format(taille,temps))
+            
+    print("temps:", sum(temps_select)) # DEBUG
+    os.system('gnuplot --persist temps_tri_selec.gplot')
+    
+    meil_cas = []
+    cas_moy = []
+    pire_cas = []
+    for taille in range(0, TAILLE_MAX+1):
+        l = liste_alea(taille)
+        temps_meil_cas = timeit.timeit(stmt='tris.tri_insert(l)',
+                                      setup="import tris; from __main__ import liste_alea, l,  taille; from random import shuffle",
+                                      number=NUMBER_ITERATIONS)
+        meil_cas.append(temps_meil_cas)
+    print(sum(meil_cas))
 
-##print(temps_select)
-print(sum(temps_select))
-os.system('gnuplot --persist temps_tri_selec.gplot')
+    for taille in range(0, TAILLE_MAX+1):
+        l= liste_alea(taille)
+        temps_tri_melange = timeit.timeit(stmt='tris.tri_insert(l); shuffle(l)',
+                                      setup="import tris; from __main__ import liste_alea, l, taille; from random import shuffle",
+                                      number=NUMBER_ITERATIONS)
+        temps_melange = timeit.timeit(stmt='shuffle(l)',
+                                      setup="import tris; from __main__ import liste_alea, l, taille; from random import shuffle",
+                                      number=NUMBER_ITERATIONS)
+        cas_moy.append(temps_tri_melange - temps_melange)
+    print(sum(cas_moy))
+    
+    for taille in range(0, TAILLE_MAX+1):
+        l = sorted(liste_alea(taille), reverse=True)
+        temps_tri_reverse = timeit.timeit(stmt='tris.tri_insert(l); l.sort(reverse=True)',
+                                          setup="import tris; from __main__ import liste_alea, l, taille; from random import shuffle",
+                                          number=NUMBER_ITERATIONS)      
+        temps_reverse = timeit.timeit(stmt='l.sort(reverse=True)',
+                                          setup="import tris; from __main__ import liste_alea, l, taille; from random import shuffle",
+                                          number=NUMBER_ITERATIONS)  
+        pire_cas.append(temps_tri_reverse - temps_reverse)
+    print(sum(pire_cas))
+    
+    with open("temps_tri_insertion.csv","w",encoding="utf-8") as canal_insert:
+        canal_insert.write('taille;"meilleur cas";"cas moyen";"pire cas"\n')
+        for i in range(TAILLE_MAX+1):
+            canal_insert.write("{:3d} ; {:8f} ; {:8f} ; {:8f}\n".format(i, meil_cas[i], cas_moy[i], pire_cas[i]))
+    os.system('gnuplot --persist temps_tri_insertion.gplot')
+        
+        
+    
 
 
 
